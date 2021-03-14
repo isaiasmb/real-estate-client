bf245dddd3e6b7ac6f02722bea529090c77d7b21
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2021-03-14 19:31:16 +0100

    Create common functions

---
+++ src/components/common/index.ts
@@ -0,0 +1,76 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react'
+
+export const useMouseDown = (
+  nodes: React.RefObject<HTMLDivElement> | React.RefObject<HTMLDivElement>[],
+  callback: () => void,
+  propsToWait: (string | number | boolean)[] = []
+) => {
+  const handleClick = (event: MouseEvent) => {
+    console.log('event: ', event)
+    if (Array.isArray(nodes)) {
+      const resultNodes = nodes.map((node) => {
+        return containsTarget(event, node)
+      })
+
+      if (
+        callback &&
+        typeof callback === 'function' &&
+        !resultNodes.includes(true)
+      ) {
+        callback()
+      }
+    } else {
+      const contains = containsTarget(event, nodes)
+      if (callback && typeof callback === 'function' && !contains) {
+        callback()
+      }
+    }
+  }
+
+  const containsTarget = (
+    event: MouseEvent,
+    node: React.RefObject<HTMLDivElement>
+  ) => {
+    if (!node || !node.current) {
+      return false
+    }
+    if (node.current.contains(<Node>event.target)) {
+      return true
+    }
+    return false
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick)
+    }
+  }, propsToWait)
+}
+
+export const useScroll = (
+  id: string,
+  callback: () => void,
+  propsToWait: string[] = []
+) => {
+  const handleScroll = () => {
+    if (callback && typeof callback === 'function') {
+      callback()
+    }
+  }
+
+  useEffect(() => {
+    const el = document.getElementById(id)
+    if (!el) {
+      return
+    }
+
+    el.addEventListener('scroll', handleScroll)
+
+    return () => {
+      el.removeEventListener('scroll', handleScroll)
+    }
+  }, propsToWait)
+}
