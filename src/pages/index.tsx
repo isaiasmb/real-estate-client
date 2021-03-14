e25f4ab31f1b3312743f1635fd9391ea119d1a65
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2021-03-14 19:41:20 +0100

    Add Home template to the index of the application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
-import Main from 'components/Main'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import houseCardsMock from 'components/HouseCard/mock'
 
-export default function Home() {
-  return <Main />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      recentProperties: houseCardsMock
+    }
+  }
 }
