c76fac245b9464a62233cc9e621f47205b255827
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2021-04-14 22:52:29 +0200

    Create Textarea component

---
+++ src/components/Textarea/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Textarea = () => (
+  <S.Wrapper>
+    <h1>Textarea</h1>
+  </S.Wrapper>
+)
+
+export default Textarea
---
+++ src/components/Textarea/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Textarea from '.'
+
+export default {
+  title: 'Textarea',
+  component: Textarea
+} as Meta
+
+export const Default: Story = () => <Textarea />
