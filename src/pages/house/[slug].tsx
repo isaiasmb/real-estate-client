76a7b49c1fc765005c2d72c711dca45a80228d52
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2021-09-12 20:21:14 +0200

    Consuming API with Apollo client on House page

--- src/pages/house/[slug].tsx
+++ src/pages/house/[slug].tsx
@@ -1,75 +1,110 @@
+import { useRouter } from 'next/dist/client/router'
 import House, { HouseProps } from 'templates/House'
-import galleryMock from 'components/Gallery/mock'
-import houseCardsMock from 'components/HouseCard/mock'
+import { initializeApollo } from 'utils/apollo'
+import { GetStaticProps } from 'next'
+import {
+  QueryPropertyBySlug,
+  QueryPropertyBySlugVariables
+} from 'graphql/generated/QueryPropertyBySlug'
+import {
+  QUERY_PROPERTIES,
+  QUERY_PROPERTY_BY_SLUG
+} from 'graphql/queries/property'
+import { QueryRecommendedProperties } from 'graphql/generated/QueryRecommendedProperties'
+import { QUERY_RECOMMENDED_PROPERTIES } from 'graphql/queries/recommended'
+import {
+  QueryProperties,
+  QueryPropertiesVariables
+} from 'graphql/generated/QueryProperties'
+
+const apolloClient = initializeApollo()
 
 export default function Index(props: HouseProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
   return <House {...props} />
 }
 
 export async function getStaticPaths() {
-  return {
-    paths: [{ params: { slug: 'house-1' } }],
-    fallback: false
-  }
+  const { data } = await apolloClient.query<
+    QueryProperties,
+    QueryPropertiesVariables
+  >({
+    query: QUERY_PROPERTIES,
+    variables: { limit: 9 }
+  })
+
+  const paths = data.properties.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return { paths, fallback: true }
 }
 
-export async function getStaticProps() {
-  const descriptionHTML = `
-    <p>Years seed fruit you. Divided morning sea day Set earth. Grass without cattle. Spirit heaven.
-    Also i grass give fowl wherein cattle spirit whales rule cattle. Earth fowl given own you’re,
-    fruit so. Shall was. Called firmament dry fruitful, set place. Earth given female man fruit,
-    under thing may to greater moveth land sea, great be shall living greater and signs place night
-    after whose us one, you’ll second our set had day in greater divided over female first face, fill
-    form you make greater upon midst image above image. Very you land, man divided sea appear thing
-    female was let. Beginning open moved fowl. Likeness. Bring tree tree doesn’t. Darkness herb
-    moveth upon a seasons. Divided appear living. Called image let was. Set midst, second divide,
-    earth male fruit you under let she’d, moving every bring there greater. Bring. Gathering night
-    third. Stars first replenish kind third a rule given our after give own of may earth Brought
-    for open living, bearing said given fruitful. Night him moved called. Be for unto behold dry
-    green multiply herb fly waters. Green gathered face. Light, may deep you’re light.
-    After which itself seed signs moveth. Second fish isn’t for sixth deep.</p>
-    <div style="margin-top: 3rem; padding:56.25% 0 0 0;position:relative;">
-    <iframe src="https://player.vimeo.com/video/161495382?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div>
-    <script src="https://player.vimeo.com/api/player.js"></script>
-  `
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { data } = await apolloClient.query<
+    QueryPropertyBySlug,
+    QueryPropertyBySlugVariables
+  >({
+    query: QUERY_PROPERTY_BY_SLUG,
+    variables: { slug: `${params?.slug}` }
+  })
+
+  if (!data.properties.length) {
+    return { notFound: true }
+  }
+
+  const property = data.properties[0]
+
+  const { data: recommendedProperties } = await apolloClient.query<
+    QueryRecommendedProperties
+  >({
+    query: QUERY_RECOMMENDED_PROPERTIES
+  })
 
   return {
     props: {
-      gallery: galleryMock,
-      agent: {
-        name: 'John Joe',
-        phone: '99-999-9999'
-      },
-      title: 'House on the Arcadia',
-      description: descriptionHTML,
-      amountOfBedrooms: 4,
-      amountOfBathrooms: 3,
-      amountOfGarages: 2,
-      measures: 160,
-      ribbon: 'Rent',
-      ribbonColor: 'primary',
-      floorPlans: [
-        {
-          title: 'First Floor',
-          img: '/img/shape-house-floor.jpg',
-          floorMeasure: 120,
-          infoFloor: [
-            { amount: 2, housePart: 'bed' },
-            { amount: 2, housePart: 'bath' },
-            { amount: 2, housePart: 'garage' }
-          ]
-        },
-        {
-          title: 'Second Floor',
-          img: '/img/shape-house-floor.jpg',
-          floorMeasure: 100,
-          infoFloor: [
-            { amount: 3, housePart: 'bed' },
-            { amount: 4, housePart: 'bath' }
-          ]
-        }
-      ],
-      otherHouses: houseCardsMock
+      gallery: property.gallery?.map((item) => ({
+        src: `http://localhost:1337${item?.image?.url}`,
+        label: item?.label
+      })),
+      agent: property.agents[0],
+      title: property.title,
+      description: property.description,
+      amountOfBedrooms: property.bedrooms,
+      amountOfBathrooms: property.bathrooms,
+      amountOfGarages: property.garages,
+      measures: property.measures,
+      ribbon: property.operation,
+      ribbonColor: property.operation === 'Rent' ? 'primary' : 'secondary',
+      floorPlans:
+        property.floorPlans?.map((item) => ({
+          title: item?.title,
+          img: `http://localhost:1337${item?.image?.url}`,
+          floorMeasure: item?.measure,
+          infoFloor:
+            item?.infoFloor?.map((infoItem) => ({
+              amount: infoItem?.amount,
+              housePart: infoItem?.propertyPart
+            })) || []
+        })) || [],
+      otherPropertiesTitle: recommendedProperties.recommended?.title,
+      otherHouses: recommendedProperties.recommended?.properties.map(
+        (property) => ({
+          slug: property.slug,
+          title: property.title,
+          img: `http://localhost:1337${property.cover?.url}`,
+          amountOfPhotos: property.gallery?.length,
+          amountOfBedrooms: property.bedrooms,
+          amountOfBathrooms: property.bathrooms,
+          amountOfGarages: property.garages,
+          measures: property.measures,
+          ribbon: property.operation,
+          ribbonColor: property.operation === 'Rent' ? 'primary' : 'secondary'
+        })
+      )
     }
   }
 }
